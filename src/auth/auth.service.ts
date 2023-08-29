cd9d9dde6ede3c8c02ff97e9516cc35f8bd4407c
b0nz <elliantosarono.p@gmail.com>
2023-08-29 22:28:19 +0700

    update auth controller & service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signIn() {
+    return { message: "i've signed in" };
+  }
+
+  signUp() {
+    return { message: "i've signed up" };
+  }
+}
