2e21cdd4b6269d4b1e3a1456b2404c4e33d27360
b0nz <elliantosarono.p@gmail.com>
2023-08-31 00:00:03 +0700

    add bookmark & user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BookmarkController } from './bookmark/bookmark.controller';
+import { BookmarkService } from './bookmark/bookmark.service';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  controllers: [AppController, BookmarkController],
+  providers: [AppService, BookmarkService],
 })
 export class AppModule {}
